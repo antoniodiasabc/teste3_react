b18a86063964b627e46b0478473acaa9f02a625c
Antonio <adias@atech.com.br>
2022-05-07 21:48:47 -0300

    1commit

---
+++ src/Greeting.js
@@ -0,0 +1,8 @@
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+  }
+  export default Greeting;
